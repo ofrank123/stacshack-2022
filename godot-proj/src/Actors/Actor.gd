c14016bfdba57b157fc4f036086a1920ab0070be
ynl1 <ynl1@st-andrews.ac.uk>
2022-03-05 16:59:48 +0000

    test room map

--- godot-proj/src/Actors/Actor.gd
+++
@@ -1,7 +0,0 @@
-extends KinematicBody2D
-class_name Actor
-
-#physics process runs every frame - you should store player movements here
-func _physics_process(delta: float) -> void: 
-	var velocity: = Vector2(300,0)
-	move_and_slide(velocity)
--- godot-proj/src/Actors/Player.gd
+++ godot-proj/src/Actors/Player.gd
@@ -1,1 +1,28 @@
-extends Actor
+extends KinematicBody2D
+
+var velocity : Vector2 = Vector2()
+var direction : Vector2 = Vector2()
+var speed : = 694.20
+
+func read_input():
+	velocity = Vector2()
+	if Input.is_action_pressed("up"):
+		velocity.y -= 1
+		direction = Vector2(0,-1)
+	if Input.is_action_pressed("down"):
+		velocity.y += 1
+		direction = Vector2(0,1)
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+		direction = Vector2(-1,0)
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+		direction = Vector2(1,0)
+	
+	#if two keys pressed at once, power of vector comes from how hard you are pressing the keys
+	velocity = velocity.normalized()
+	velocity = move_and_slide(velocity*speed) #200 is the speed
+
+func _physics_process(delta: float) -> void: #runs every frame--player movement dealt with here
+	read_input()
+	
